2779e4e5a9a8a7a4077af6ca44adfc50f834a64d
Jay Schwane <jrschwane@uwalumni.com>
2017-12-12 08:58:07 -0600

    Adds check for prod vs dev and sets API url accordingly

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const isDev   = process.env.NODE_ENV === 'development';
+const devUrl  = 'http://localhost:8081';
+const prodUrl = 'https://co-ment-admin-dash.glitch.me/';
+const APIurl  = isDev ? devUrl : prodUrl;
+
 export default () => {
     return axios.create({
-        baseURL: 'http://localhost:8081'
+        baseURL: APIurl
     });
 };
