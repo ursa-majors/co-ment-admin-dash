28a31bc4476606c9dcf593ccffcbbec1e0ecc6f5
Jay Schwane <jrschwane@uwalumni.com>
2017-12-13 12:52:55 -0600

    Adds method docs; Syntax formatting fixes

--- client/src/components/DashBodyNav.vue
+++ client/src/components/DashBodyNav.vue
@@ -20,98 +20,108 @@
 </template>
 
 <script>
-    import { logout } from '@/utils/auth';
+import { logout } from "@/utils/auth";
 
-    export default {
-        data() {
-            return {
-                nav_links: [
-                    'users', 'posts', 'connections', 'conversations', 'logs'
-                ]
-            };
+export default {
+    data() {
+        return {
+            nav_links: ["users", "posts", "connections", "conversations", "logs"]
+        };
+    },
+    methods: {
+
+        /** HANDLE LINK
+         *  Route to link on nav button click - WIP
+         *  @param     {String}   link   Collection name to route to
+         *  @returns   {null}
+        */
+        handleLink(link) {
+            alert(link);
         },
-        methods: {
-            handleLink(link) {
-                alert(link);
-            },
-            handleLogout() {
-                logout();
-            }
+
+        /** HANDLE LOGOUT
+         *  Auth utility 'logout' method deletes JWT and routes to '/'
+         *  @returns   {null}
+        */
+        handleLogout() {
+            logout();
         }
+
     }
+};
 </script>
 
 <style lang="css">
-    .dash-nav {
-        flex: 0 0 40px;
-        background: #444;
-        color: white;
-        box-shadow: 1px 1px 4px #444;
-    }
+.dash-nav {
+    flex: 0 0 40px;
+    background: #444;
+    color: white;
+    box-shadow: 1px 1px 4px #444;
+}
 
-    .dash-nav ul {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        height: 100%;
-        display: flex;
-        flex-flow: column nowrap;
-        justify-content: space-between;
-    }
+.dash-nav ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-between;
+}
 
-    .dash-nav-links button {
-        background: transparent;
-        width: 100%;
-        padding: 0;
-        box-sizing: border-box;
-        display: block;
-        margin-bottom: 1.5em;
-        border: 4px solid transparent;
-    }
+.dash-nav-links button {
+    background: transparent;
+    width: 100%;
+    padding: 0;
+    box-sizing: border-box;
+    display: block;
+    margin-bottom: 1.5em;
+    border: 4px solid transparent;
+}
 
-    .dash-nav-links button:hover,
-    .dash-nav-links button:active {
-        cursor: pointer;
-        border: 4px solid #3ec1c1;
-    }
+.dash-nav-links button:hover,
+.dash-nav-links button:active {
+    cursor: pointer;
+    border: 4px solid #3ec1c1;
+}
 
-    .dash-nav-links button img {
-        display: block;
-        width: 100%;
-    }
+.dash-nav-links button img {
+    display: block;
+    width: 100%;
+}
 
-    .dash-nav-links button p {
-        background: #444;
-        border-radius: .2em;
-        color: white;
-        display: block;
-        height: 20px;
-        letter-spacing: 2px;
-        left: 45px;
-        line-height: 1.3em;
-        margin-top: -31px;
-        opacity: 0;
-        padding: .4em .75em;
-        position: absolute;
-        text-transform: capitalize;
-        transition: visibility 0s, opacity 0.2s linear;
-        visibility: hidden;
-    }
+.dash-nav-links button p {
+    background: #444;
+    border-radius: 0.2em;
+    color: white;
+    display: block;
+    height: 20px;
+    letter-spacing: 2px;
+    left: 45px;
+    line-height: 1.3em;
+    margin-top: -31px;
+    opacity: 0;
+    padding: 0.4em 0.75em;
+    position: absolute;
+    text-transform: capitalize;
+    transition: visibility 0s, opacity 0.2s linear;
+    visibility: hidden;
+}
 
-    .dash-nav-links button:hover p {
-        visibility: visible;
-        opacity: 1;
-    }
+.dash-nav-links button:hover p {
+    visibility: visible;
+    opacity: 1;
+}
 
-    .dash-nav-links button.last {
-        margin-bottom: 0;
-    }
+.dash-nav-links button.last {
+    margin-bottom: 0;
+}
 
-    .dash-nav-links button.last:hover {
-        border: 4px solid #cb4747;
-    }
+.dash-nav-links button.last:hover {
+    border: 4px solid #cb4747;
+}
 
-    .dash-nav-links button.active {
-        border: 4px solid #3ec1c1;
-    }
+.dash-nav-links button.active {
+    border: 4px solid #3ec1c1;
+}
 </style>
