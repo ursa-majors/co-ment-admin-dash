2a305a5b0543c411b290dd526d6b09f5bb8d6bbf
Jay Schwane <jrschwane@uwalumni.com>
2017-12-14 14:06:14 -0600

    Adds requireAuth middleware and isLoggedIn util

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,7 +1,51 @@
+/* ================================= SETUP ================================= */
+
+import decode from 'jwt-decode';
 import router from '@/router';
 import LS     from './localStorage';
+
+
+/* ============================ UTILITY METHODS ============================ */
+
+function getTokenExpirationDate(encodedToken) {
+    const token = decode(encodedToken);
+    if (!token.exp) {
+        return null;
+    }
+
+    const date = new Date(0);
+    date.setUTCSeconds(token.exp);
+    return date;
+}
+
+
+function isTokenExpired(token) {
+    const expirationDate = getTokenExpirationDate(token);
+    return expirationDate < new Date();
+}
+
+
+/* ============================ PUBLIC METHODS ============================= */
 
 export function logout() {
     LS.deleteData('auth_token');
     router.push('/');
+}
+
+
+export function isLoggedIn() {
+    const token = LS.getData('auth_token');
+    return !!token && !isTokenExpired(token);
+}
+
+
+export function requireAuth(to, from, next) {
+    if (!isLoggedIn()) {
+        next({
+            path  : '/',
+            query : { redirect: to.fullPath }
+        });
+    } else {
+        next();
+    }
 }
